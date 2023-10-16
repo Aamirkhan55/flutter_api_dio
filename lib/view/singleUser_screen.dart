22cc819957e731165c11e15449f6799a0c9a57d5
Amir55143 <aamirnoor55143@gmail.com>
2023-10-16 15:21:56 +0500

    User/Data

--- lib/view/singleUser_screen.dart
+++ lib/view/singleUser_screen.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_api_dio/model/singleUser.dart';
+import 'package:flutter_api_dio/model/userModel.dart';
 import 'package:flutter_api_dio/services/http_services.dart';
 
 class SingleUserScreen extends StatefulWidget {
@@ -11,36 +12,51 @@ class SingleUserScreen extends StatefulWidget {
 }
 
 class _SingleUserScreenState extends State<SingleUserScreen> {
-
   HttpServices? http;
   SingleUserResponse? singleUserResponse;
+  bool isLoading = false;
+  UserModel? user;
 
-  void getUser() async{
+  Future getUser() async {
     Response response;
-
     try {
+      isLoading = true;
       response = await http!.getRequest("/api/users/2", '');
+      isLoading = false;
       if (response.statusCode == 200) {
         setState(() {
           singleUserResponse = SingleUserResponse.fromJson(response.data);
+          user = singleUserResponse!.user;
         });
       } else {
         'There is some error';
       }
-    }on Exception catch (e) {
-     e.toString(); 
+    } on Exception catch (e) {
+      e.toString();
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Api Demo'),
       ),
-      body: const Center(
+      body: isLoading ? const Center(
         child: CircularProgressIndicator(),
-      ),
+      ) :  user != null ? ListView.builder(
+        itemBuilder: (context, index) {
+          return Card(
+            child: ListTile(
+              leading: Image.network(user!.avatar.toString()),
+              title: Text(user!.first_name.toString()),
+              subtitle: Text(user!.last_name.toString()),
+            ),
+          );
+        },
+        ) : const Center(
+        child: Text('No Data')
+        ),
     );
   }
 }
